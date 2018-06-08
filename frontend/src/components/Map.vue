84a941f7cbaa0ce12b61d875d801303e06b9c51e
Phm Ngoc Nghia <pmhngocnghia@outlook.com>
2018-06-08 21:14:18 +0700

    update websocket + connect frontend to websocket

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -10,6 +10,7 @@
 <script>
 let map = null
 let locationSearchMarker = null
+let trafficReports = []
 let directionsService = new window.google.maps.DirectionsService()
 let directionsDisplay = new window.google.maps.DirectionsRenderer()
 
@@ -61,13 +62,74 @@ export default {
     },
 
     onSearchRouteCleared () {
-      directionsDisplay.set('directions', null);
+      directionsDisplay.set('directions', null)
     },
 
     clearMarkers (markers) {
       for (let marker of markers) {
         marker.setMap(null)
       }
+    },
+
+    parseBoundsIntoLocation (bounds) {
+      const southWest = bounds.getSouthWest().toJSON()
+      const northEast = bounds.getNorthEast().toJSON()
+      return {
+        fromLat: southWest.lat,
+        toLat: northEast.lat,
+        fromLng: southWest.lng,
+        toLng: northEast.lng
+      }
+    },
+
+    addTrafficReport (trafficReport) {
+      trafficReport.marker = new window.google.maps.Marker({
+        position: {
+          lat: trafficReport.location.lat,
+          lng: trafficReport.location.lng
+        },
+        map: map
+      })
+
+      trafficReports.push(trafficReport)
+    },
+
+    deleteTrafficReport (trafficReport) {
+      /**
+       * Find traffic report from arr
+       * Remove from map
+       * remove from arr
+       */
+      let index = this.findIndexTrafficReport(trafficReport)
+      if (index === -1) {
+        /**
+         * Never happend
+         */
+        return
+      }
+
+      trafficReports[index].marker.setMap(null)
+      trafficReports.splice(index, 1)
+    },
+
+    findIndexTrafficReport (trafficReport) {
+      return trafficReports.findIndex((e) => e.id === trafficReport.id)
+    },
+
+    editTrafficReport (trafficReport) {
+      debugger
+      let index = this.findIndexTrafficReport(trafficReport)
+      if (index === -1) {
+        /**
+         * Never happend
+         */
+        return
+      }
+
+      /**
+       * Assign everything from new marker to old marker using object assign
+       */
+      trafficReports[index] = Object.assign(trafficReports[index], trafficReport)
     }
   },
 
@@ -83,6 +145,39 @@ export default {
 
     directionsDisplay.setMap(map)
     directionsDisplay.setPanel(document.getElementById('directionPanel'))
+
+    /**
+     * Init web socket
+     * Start fetch data from realtime
+     */
+    this.websocket = new WebSocket('ws://localhost:8082')
+    this.websocket.onopen = () => {
+      map.addListener('idle', () => {
+        const bounds = map.getBounds()
+        const data = this.parseBoundsIntoLocation(bounds)
+        this.websocket.send(JSON.stringify(data))
+      })
+
+      this.websocket.onmessage = ({data}) => {
+        let parseData = JSON.parse(data)
+        switch (parseData.type) {
+          case 'add':
+          case 'initial':
+            this.addTrafficReport(parseData.new_val)
+            break
+          case 'remove':
+            this.deleteTrafficReport(parseData.old_val)
+            break
+          case 'change':
+            this.editTrafficReport(parseData.new_val)
+            break
+        }
+      }
+    }
+  },
+
+  destroyed () {
+    this.websocket.close()
   }
 }
 </script>
--- websocket/index.js
+++ websocket/index.js
@@ -19,18 +19,48 @@ async function start () {
        * WARNING : doesn't validate payload
        */
       client.on('message', (payload) => {
-        console.log(payload)
+        const filterData = JSON.parse(payload)
+
+        /**
+         * Send all data base on filter
+         *
+
         /**
          * Subscribe rethink data base on message request
+         * Send all data base on update
          */
-        // r.table('activeTrafficReport')
-        //   .filter()
-        //   .changes()
-        //   .run(conn, (err, cursor) => {
-        //     if (err) {
-        //       throw err
-        //     }
-        //   })
+        r.table('activeTrafficReports')
+          .changes({
+            'includeTypes': true,
+            'includeInitial': true,
+            "squash": true
+          })
+          .run(conn, async (err, cursor) => {
+            if (err) {
+              throw err
+            }
+
+            cursor.each((err, row) => {
+              if (err) {
+                throw err
+              }
+
+              client.send(JSON.stringify(row))
+            })
+
+            /**
+             * Close stream when :
+             * Client disconnect
+             * Client send new message
+             */
+            wss.on('close', () => {
+              cursor.close()
+            })
+
+            client.on('message', () => {
+              cursor.close()
+            })
+          })
       })
     })
   } catch (err) {
